480bc2cabfa2c5b4c33fced3174ae8208593bc86
mireal <moveloader@yandex.ru>
2025-02-05 15:08:56 +0300

    contact form fix

--- src/components/features/ContactForm.tsx
+++ src/components/features/ContactForm.tsx
@@ -1,6 +1,6 @@
-import { Input, Stack, styled } from "@mui/material";
+import { Box, Input, styled } from "@mui/material";
 import StyledButton from "../shared/StyledButton";
-import { Form, Formik } from "formik";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 
 const InputField = styled(Input)(({ theme }) => ({
@@ -20,13 +20,6 @@ const InputField = styled(Input)(({ theme }) => ({
   },
 }));
 
-// interface IFormInput {
-//     name: string
-//     phone: string
-//     email: string
-//     message: string
-// }
-
 const initialValues = {
   name: "",
   phone: "",
@@ -35,48 +28,83 @@ const initialValues = {
 };
 
 const validationSchema = Yup.object({
-  name: Yup.string().required("Name is required"),
-  phone: Yup.string().required("Phone is required"),
+  name: Yup.string().min(2, "Invalid name").required("Name is required"),
+  phone: Yup.string()
+    .min(8, "Invalid phone number")
+    .required("Phone is required"),
   email: Yup.string()
     .email("Invalid email address")
     .required("Email is required"),
-  message: Yup.string().required("Message is required"),
+  message: Yup.string()
+    .min(10, "Message is too short")
+    .required("Message is required"),
 });
 
 export default function ContactForm() {
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: validationSchema,
+    onSubmit: (values, actions) => {
+      console.log({ values, actions });
+      // alert(JSON.stringify(values, null, 2));
+      actions.setSubmitting(false);
+    },
+  });
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={(values, actions) => {
-        console.log({ values, actions });
-        alert(JSON.stringify(values, null, 2));
-        actions.setSubmitting(false);
-      }}
-    >
-      <Form>
-        <Stack gap="15px">
-          <InputField id="name" name="name" placeholder="NAME" />
-          <InputField id="email" name="email" placeholder="EMAIL" />
-          <InputField id="phone" name="phone" placeholder="PHONE NUMBER" />
-          <InputField
-            id="message"
-            name="message"
-            placeholder="MESSAGE"
-            multiline
-            sx={{
-              minHeight: "200px",
-              display: "flex",
-              alignItems: "start",
-            }}
-          />
+    <>
+      <Box
+        component="form"
+        onSubmit={formik.handleSubmit}
+        display="flex"
+        flexDirection="column"
+        gap="15px"
+      >
+        <InputField
+          id="name"
+          name="name"
+          value={formik.values.name}
+          error={formik.touched.name && !!formik.errors.name}
+          onChange={formik.handleChange}
+          placeholder="NAME"
+        />
 
-          <StyledButton fullWidth type="submit" sx={{ mt: "15px" }}>
-            {" "}
-            SUBMIT{" "}
-          </StyledButton>
-        </Stack>
-      </Form>
-    </Formik>
+        <InputField
+          id="email"
+          name="email"
+          value={formik.values.email}
+          error={formik.touched.email && !!formik.errors.email}
+          onChange={formik.handleChange}
+          placeholder="EMAIL"
+        />
+
+        <InputField
+          id="phone"
+          name="phone"
+          value={formik.values.phone}
+          error={formik.touched.phone && !!formik.errors.phone}
+          onChange={formik.handleChange}
+          placeholder="PHONE NUMBER"
+        />
+
+        <InputField
+          id="message"
+          name="message"
+          value={formik.values.message}
+          error={formik.touched.message && !!formik.errors.message}
+          onChange={formik.handleChange}
+          placeholder="MESSAGE"
+          multiline
+          sx={{
+            minHeight: "200px",
+            display: "flex",
+            alignItems: "start",
+          }}
+        />
+
+        <StyledButton fullWidth type="submit" sx={{ mt: "15px" }}>
+          SUBMIT
+        </StyledButton>
+      </Box>
+    </>
   );
 }
