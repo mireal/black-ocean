6317c0e84aa512a00a11b6d63f6b76140ed6b551
mireal <moveloader@yandex.ru>
2025-02-05 12:55:06 +0300

    removed target blank from logo link

--- src/components/entities/NavbarLogo.tsx
+++ src/components/entities/NavbarLogo.tsx
@@ -4,7 +4,7 @@ import { Link as RouterLink } from "react-router-dom";
 export default function NavbarLogo() {
   return (
     <>
-      <Link component={RouterLink} to="/" target="_blank">
+      <Link component={RouterLink} to="/">
         <img
           src="../assets/icons/black_ocean_logo.svg"
           className="logo"
